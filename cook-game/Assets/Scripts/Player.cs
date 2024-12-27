64087f67c79cc277e9d922b3b67f5c31467b4d65
Maciej Potręć <potrec123@proton.me>
2024-12-27 23:39:16 +0100

    ref: add unity input system with support for gamepads

--- cook-game/Assets/Scripts/Player.cs
+++ cook-game/Assets/Scripts/Player.cs
@@ -4,29 +4,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotationSpeed = 20f;
+    [SerializeField] private GameInput gameInput;
     
     private bool isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveVector = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveVector != Vector3.zero;
