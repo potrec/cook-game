acfc6a15332865e92c66a0651c21310e0895af1c
Maciej Potręć <potrec123@proton.me>
2024-12-28 02:11:56 +0100

    feat: add collision detection to player

--- cook-game/Assets/Scripts/Player.cs
+++ cook-game/Assets/Scripts/Player.cs
@@ -11,12 +11,44 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        
         Vector3 moveVector = new Vector3(inputVector.x, 0f, inputVector.y);
-        isWalking = moveVector != Vector3.zero;
-        moveVector *= Time.deltaTime * moveSpeed;
         
-        transform.position += moveVector;
+        float playerHeight = 2f;
+        float playerRadius = 0.7f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveVector, moveDistance);
+        
+        if(!canMove)
+        {
+            Vector3 moveVectorX = new Vector3(moveVector.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveVectorX, moveDistance);
+
+            if (canMove)
+            {
+                moveVector = moveVectorX;
+            }
+            else
+            {
+                Vector3 moveVectorZ = new Vector3(0f, 0f, moveVector.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveVectorZ, moveDistance);
+                if (canMove)
+                {
+                    moveVector = moveVectorZ;
+                }
+                else
+                {
+                    moveVector = Vector3.zero;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveVector * moveDistance;
+        }
+        
+        isWalking = moveVector != Vector3.zero;
+        
         transform.forward = Vector3.Slerp(transform.forward,moveVector,Time.deltaTime*rotationSpeed);
     }
     
