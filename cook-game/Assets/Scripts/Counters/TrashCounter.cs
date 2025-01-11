75b915e40e7a715d8afccab0b239e8413231f4f8
Maciej Potręć <potrec123@proton.me>
2025-01-11 17:23:47 +0100

    feat: add trash counter

---
+++ cook-game/Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,13 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
